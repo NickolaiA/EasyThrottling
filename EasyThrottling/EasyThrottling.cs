bab216cfe2041cd90e10e6cb89809aaead661cf7
NickolaiA <NickolaiA@gmail.com>
2016-06-25 16:18:34 +1000

    The first checking of the code

---
+++ EasyThrottling/EasyThrottling.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace EasyThrottling
+{
+    public enum ThrottleState { Allowed, Busy, Continue }
+
+    public class Throttle
+    {
+        private readonly object syncRoot = new object();
+        private readonly int _limit;
+        private readonly TimeSpan _duration;
+        private DateTime _next = DateTime.MinValue;
+        private int _currentItems;
+
+        public delegate void NotifyDelegate(ThrottleState state, int numberOfItemsInProgress, int limit);
+        public event NotifyDelegate OnNotify;
+
+        public int CurrentItems
+        {
+            get { return _currentItems; }
+        }
+
+        public Throttle(TimeSpan duration, int limit)
+        {
+            this._duration = duration;
+            this._limit = limit;
+        }
+
+        public void Wait()
+        {
+            lock (syncRoot)
+            {
+                DateTime now = DateTime.UtcNow;
+
+                if (_next < now)
+                {
+                    _currentItems = 0; _next = now + _duration;
+                }
+
+                ++_currentItems;
+
+                if (_currentItems <= _limit)
+                {
+                    Notify(ThrottleState.Allowed, _currentItems, _limit);
+                    return;
+                }
+                else
+                {
+                    Notify(ThrottleState.Busy, _currentItems, _limit);
+                }
+
+                Thread.Sleep(_next - now);
+                _next = DateTime.UtcNow + _duration;
+                _currentItems = 1;
+                Notify(ThrottleState.Continue, _currentItems, _limit);
+            }
+        }
+
+        private void Notify(ThrottleState state, int numberOfItemsInProgress, int limit)
+        {
+            if (OnNotify != null)
+            {
+                OnNotify(state, numberOfItemsInProgress, limit);
+            }
+        }
+    }
+}
---
+++ EasyThrottling/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyThrottling
+{
+    public class Program
+    {
+        public const int MaxTasks = 3;
+        public const int MaxIterations = 5;
+
+        static void Main(string[] args)
+        {
+            TaskData data;
+            Throttle throttle = new Throttle(new TimeSpan(0, 0, 2), MaxTasks);
+            throttle.OnNotify += Throttle_OnNotify;
+
+            for (int i = 1; i <= 5; i++) {
+                data = new TaskData { TaskId = i, Throttle = throttle };
+                Task task = new Task(t => DoSomething((TaskData)t), data);
+                task.Start();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Throttle_OnNotify(ThrottleState state, int numberOfItemsInProgress, int limit)
+        {
+            if (state == ThrottleState.Allowed || state == ThrottleState.Continue)
+            {
+                Console.WriteLine(string.Format("{0} - {1} of {2} tasks in progress", state.ToString(), numberOfItemsInProgress, limit));
+            } else
+            {
+                Console.WriteLine(string.Format("{0} - Waiting before executing the next task... Number of active tasks {1}", state.ToString(), numberOfItemsInProgress- 1));
+            }
+        }
+
+        private static void DoSomething(TaskData data)
+        {
+            for (int i=1; i <= MaxIterations; i++)
+            {
+                data.Throttle.Wait();
+
+                Console.WriteLine(string.Format("Task #{0} iteration {1}", data.TaskId, i));
+                Thread.Sleep(3000);
+            }
+
+            Console.WriteLine(string.Format("Finish task #{0}", data.TaskId));
+        }
+    }
+
+    public class TaskData
+    {
+        public int TaskId { get; set; }
+        public Throttle Throttle;
+    }
+}
